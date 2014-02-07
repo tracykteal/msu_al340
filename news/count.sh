004b12b47ab055865cddc6e141280139fa41b65b
Tracy K. Teal <tkteal@gmail.com>
2014-02-07 07:17:02 -0500

    Added comments to count.sh

--- news/count.sh
+++ news/count.sh
@@ -2,8 +2,10 @@
 
 echo 'What word do you want to search for?'
 
+# Read in the word 
 read WORD
 
+# Look for the word in the file and count how many times it occurs
 for i in $( ls *.txt); do
   echo $i Counts for the word \'$WORD\': 
   grep $WORD $i | wc -l
